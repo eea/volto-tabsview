afdb8c347d9798f2acb8088e94699e09bcf9dc4e
Miu Razvan <miu.razvan28@gmail.com>
2020-07-29 14:28:43 +0300

    Removed unused components

--- config.js
+++ config.js
@@ -1,15 +1,13 @@
+/* MOSAIC */
+import MosaicForm from 'volto-mosaic/components/manage/Form';
+/* LOCAL SETTINGS */
 import * as addonReducers from './reducers';
 import addonRoutes from './routes.js';
-
-import MosaicTilesView from './components/theme/View';
-import MosaicForm from './components/manage/Form';
-import ClonedBlocksView from './components/theme/ClonedBlocksView';
-import TabsMosaicView from './components/theme/Tabs/View';
-import TabsMosaicChildView from './components/theme/Tabs/View';
+/* VIEWS */
+import TabsMosaicView from './components/theme//View';
+import TabsMosaicChildView from './components/theme/View';
 
 export function applyConfig(config) {
-  config.settings.nonContentRoutes.push('/mosaic-settings-view');
-
   return {
     ...config,
     addonReducers: {
@@ -20,8 +18,6 @@ export function applyConfig(config) {
       ...config.views,
       layoutViews: {
         ...config.views.layoutViews,
-        cloned_blocks_view: ClonedBlocksView,
-        mosaic_tiles_view: MosaicTilesView,
         tabs_mosaic_view: TabsMosaicView,
         tabs_mosaic_child_view: TabsMosaicChildView,
       },
@@ -30,14 +26,8 @@ export function applyConfig(config) {
       ...config.editForms,
       byLayout: {
         ...config.editForms?.byLayout,
-        cloned_blocks_view: MosaicForm,
-        mosaic_tiles_view: MosaicForm,
         tabs_view_mosaic: MosaicForm,
         tabs_mosaic_child_view: MosaicForm,
-      },
-      byType: {
-        ...config.editForms?.byType,
-        'Plone Site': MosaicForm,
       },
     },
     addonRoutes: [...(config.addonRoutes || []), ...addonRoutes],
